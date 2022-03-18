5f95cf4a594444b55547e99d97f34a267da4e0f8
Vasil Hristov <vasil.hristov@netea.bg>
2022-03-18 18:34:23 +0200

    production build

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function paintings()
+    {
+        $this->hasMany(Painting::class)->value('user_id');
+    }
 }
