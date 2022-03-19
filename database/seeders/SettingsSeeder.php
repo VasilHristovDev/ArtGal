99d37d0dd94019e151b4c4b7a1394289728f4d21
Vasil Hristov <vasil.hristov@netea.bg>
2022-03-19 23:53:59 +0200

    added logo1

--- database/seeders/SettingsSeeder.php
+++ database/seeders/SettingsSeeder.php
@@ -15,23 +15,7 @@ class SettingsSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('settings')->insert([
-            'key' => 'name',
-            'name' => 'Website Name',
-            'description' => 'The name of the website',
-            'value' => 'ArtGal',
-            'field' => '{"name":"value","label":"Value","type":"tinymce"}',
-            'active' => 1
-        ]);
 
-        DB::table('settings')->insert([
-            'key' => 'description',
-            'name' => 'Website Description',
-            'description' => 'The description of the website',
-            'value' => 'Our website aims to make art popular among more and more people. We value small artists and want to help them show their art to the world!',
-            'field' => '{"name":"value","label":"Value","type":"tinymce"}',
-            'active' => 1
-        ]);
 
         DB::table('settings')->insert([
             'key' => 'logo',
