e0d489d415afe155321cf37db17f5cb78223a3d9
Vasil Hristov <vasil.hristov@netea.bg>
2022-03-20 04:22:33 +0200

    fix on resource

--- app/Http/Resources/PaintingResource.php
+++ app/Http/Resources/PaintingResource.php
@@ -18,7 +18,7 @@ class PaintingResource extends JsonResource
         $arrWithImages = [];
         foreach ($this->images as $image)
         {
-            $arrWithImages = $image->url;
+            $arrWithImages[] = $image->url;
         }
 
         return [
